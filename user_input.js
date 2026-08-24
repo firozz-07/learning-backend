ede96b2824e1648110a6eca293a260d6e4051181
firoz <firozz0007@gmail.com>
2026-08-24 19:48:23 +0530

    took input from the user for the first time

---
+++ user_input.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Complete Coding</title></head>");
+    res.write("<body><h1>Enter Your Details:</h1>");
+    res.write('<form action="/submit-details" method="POST">');
+    res.write(
+      '<input type="text" name="username" placeholder="Enter your name"><br>',
+    );
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male" />');
+    res.write('<label for="female">Female</label>');
+    res.write(
+      '<input type="radio" id="female" name="gender" value="female" />',
+    );
+    res.write('<br><input type="submit" value="Submit">');
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head>");
+  res.write("<title>jdkd</title>");
+  res.write("</head>");
+  res.write("<body>");
+  res.write("<h1>submission Donee !!!</h1>");
+  res.write("</>");
+});
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("server started");
+});
