d1ad8818e0417220f126ecf4ea218c67823be824
firoz <firozz0007@gmail.com>
2026-08-25 18:52:07 +0530

    took input from the user and converted it to the string and displayed

---
+++ parsing_req.js
@@ -0,0 +1,47 @@
+const { log } = require('console');
+const http=require('http');
+const { buffer } = require('stream/consumers');
+const server=http.createServer((req,res)=>{
+if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>chitaaa</title></head>");
+    res.write("<body><h1>Enter Your Details:</h1>");
+    res.write('<form action="/submit-details" method="POST">');
+    res.write(
+      '<input type="text" name="username" placeholder="Enter your name"><br>',
+    );
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male" />');
+    res.write('<label for="female">Female</label>');
+    res.write(
+      '<input type="radio" id="female" name="gender" value="female" />',
+    );
+    res.write('<br><input type="submit" value="Submit">');
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+   else if (req.url === "/submit-details" && req.method === "POST") {
+    const body=[];
+    req.on('data',(chunk)=>{
+      body.push(chunk);
+    });
+    req.on('end',()=>{
+      const text=Buffer.concat(body).toString();
+      console.log(text);
+    })
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Not found</h1>");
+   
+  }
+});
+const Port=3000;
+server.listen(Port);
