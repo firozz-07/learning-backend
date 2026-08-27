6fb07fd2b72ec3eb81c455c82fe6c3eacab10155
firoz <firozz0007@gmail.com>
2026-08-27 19:27:08 +0530

    done

--- express-js/node-package/app.js
+++
@@ -1,6 +0,0 @@
-const http=require('http');
-server=http.createServer((req,res)=>{
-console.log("sherr");
-});
-const Port=3000;
-server.listen(Port);
---
+++ express-js/project/app.js
@@ -0,0 +1,6 @@
+const http=require('http');
+const express=require('express');
+const app=express();
+server=http.createServer(app);
+const Port=3000;
+server.listen(Port);
