3f2e3af622dcc5f06410b82b253bd41d18c87c30
firoz <firozz0007@gmail.com>
2026-08-28 18:58:50 +0530

    started and routed the pages through expresss quite easy

--- express-js/project/app.js
+++ express-js/project/app.js
@@ -1,6 +1,14 @@
 const http=require('http');
 const express=require('express');
 const app=express();
+app.get('/',(req,res,next)=>{
+  res.send('<P>welcome to the home page</p>');
+});
+
+app.get('/submit',(req,res,next)=>{
+  res.send('<P>welcome to the submit page </p>');
+});
+
 server=http.createServer(app);
 const Port=3000;
 server.listen(Port);
