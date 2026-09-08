7c0f709f12d2c2734fe1be5c4abc90b9ce4dae39
firoz <firozz0007@gmail.com>
2026-09-08 21:37:04 +0530

    finallyyy it connecteddddd

--- express-js/airbnb/utils/database.js
+++ express-js/airbnb/utils/database.js
@@ -1,21 +1,27 @@
-
-
 require('dotenv').config();
+
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 
 const mongo_URL = process.env.MONGODB_URI;
+
 let _db;
+
 const mongoConnect = (callback) => {
   mongoClient.connect(mongo_URL).then(client => {
     console.log('MongoDB connected');
-    _db=client.db("airbnb");
+    _db = client.db('airbnb');
     callback();
   }).catch(err => {
     console.log('MongoDB connection error:', err);
   });
 };
-const getdb=()=>{return _db}
+
+const getdb = () => {
+  return _db;
+};
 
 module.exports = mongoConnect;
